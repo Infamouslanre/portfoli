cfe13cae42b422514dabd8381cf110c28e354d57
Infamouslanre <mdaramola92@gmail.com>
2023-08-14 03:25:48 +0100

    ahh go away

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+      <div className='content-center'>
         <Image
           className='rounded-full'
           src='/image/profile.jpg'
@@ -12,11 +12,8 @@ export default function Home() {
           width='144'
         />
         <h1 className='p-3 text-center'>
-          LANRE
+          PEEP THIS
         </h1>
-        <div>
-
-        </div>
       </div>
     </main>
   )
